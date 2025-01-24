aaf0b137cfb080ee65ed1eb00eba0ee68950eff6
Hillary Mwashi <mwashihillary@gmail.com>
2025-01-24 15:51:39 +0300

    Removed the frontend
    The frontend was not needed

--- PatientRegistryApp/src/app/shared/json-schema/index.ts
+++
@@ -1,5 +0,0 @@
-import type { SFWidgetProvideConfig } from '@delon/form';
-
-import { TestWidget } from './test/test.widget';
-
-export const SF_WIDGETS: SFWidgetProvideConfig[] = [{ KEY: TestWidget.KEY, type: TestWidget }];
--- PatientRegistryApp/src/app/shared/utils/yuan.ts
+++
@@ -1,6 +0,0 @@
-export function yuan(value: number | string, digits: number = 2): string {
-  if (typeof value === 'number') {
-    value = value.toFixed(digits);
-  }
-  return `&yen ${value}`;
-}
